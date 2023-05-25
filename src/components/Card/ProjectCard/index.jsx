a29e7b2ee61df4114509645ffacfc72431aec263
Henrique <you@example.com>
2023-05-25 18:24:52 -0300

    ProjectCard css pronto

--- src/components/Card/ProjectCard/index.jsx
+++ src/components/Card/ProjectCard/index.jsx
@@ -1,20 +1,21 @@
 import { projects } from "../../../data/projects.js"
 import githubIcon  from "../../../assets/github-icon.png"
 import { Button } from "../../Button/index.jsx"
-
+import styles from "../ProjectCard/styles.module.css"
 
 export function ProjectCard( {projectName, projectDecription } ){
     return(
-        <div>
-            <div>
-                <h2>{projectName}</h2>
+        <li className={styles.li}>
+            <div className={styles.container}>
+                <h2 className={styles.h2}>{projectName}</h2>
                 <img 
+                    className={styles.img}
                     src={githubIcon}
                     alt="Ícone github " 
                 />
             </div>
-                <p>{projectDecription}</p>
-                <Button text={'Saiba mais'}></Button>
-        </div>
+            <p className={styles.p}>{projectDecription}</p>
+            <a className={styles.a} href="">Saiba mais</a>
+        </li>
     )
 }
--- src/components/Sections/ProjectsSection/index.jsx
+++ src/components/Sections/ProjectsSection/index.jsx
@@ -1,13 +1,18 @@
 import { projects } from "../../../data/projects.js"
 import { ProjectCard } from "../../Card/ProjectCard/index.jsx"
+import styles from "../ProjectsSection/styles.module.css"
+
 
 export function ProjectSection(){
     return(
-        <section>
-            <ProjectCard projectName={projects[0].name} projectDecription={projects[0].descricao}></ProjectCard>
-            <ProjectCard projectName={projects[1].name} projectDecription={projects[1].descricao}></ProjectCard>
-            <ProjectCard projectName={projects[2].name} projectDecription={projects[2].descricao}></ProjectCard>
-            <ProjectCard projectName={projects[3].name} projectDecription={projects[3].descricao}></ProjectCard>
+        <section className={styles.section}>
+            <h2 className={styles.h2}>Projetos</h2>
+            <ul className={styles.ul}>
+                <ProjectCard projectName={projects[0].name} projectDecription={projects[0].descricao}></ProjectCard>
+                <ProjectCard projectName={projects[1].name} projectDecription={projects[1].descricao}></ProjectCard>
+                <ProjectCard projectName={projects[2].name} projectDecription={projects[2].descricao}></ProjectCard>
+                <ProjectCard projectName={projects[3].name} projectDecription={projects[3].descricao}></ProjectCard>
+            </ul>
         </section>
     )
 }
