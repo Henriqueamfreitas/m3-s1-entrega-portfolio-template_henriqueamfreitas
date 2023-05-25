474fc831c17ae6d173de83443a24c52b78c37851
Henrique <you@example.com>
2023-05-25 14:57:16 -0300

    AboutMeSection css pronto

--- src/components/Sections/AboutMeSection/index.jsx
+++ src/components/Sections/AboutMeSection/index.jsx
@@ -1,8 +1,11 @@
+import styles from "../AboutMeSection/styles.module.css"
+
 export function AboutMeSection(){
     return(
-        <section>
-            <h2>Sobre mim</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ex magna, imperdiet sodales sem 
+        <section className={styles.section}>
+            <h2 className={styles.h2}>Sobre mim</h2>
+            <p className={styles.p}>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ex magna, imperdiet sodales sem 
                quis, sollicitudin lobortis purus. Etiam a ipsum finibus, dictum leo non, ultrices dui. Nunc id 
                felis pharetra, vehicula enim in, suscipit nisi. Mauris eget sapien a velit facilisis ullamcorper 
                feugiat nec orci. Duis ac iaculis turpis, at feugiat orci.
