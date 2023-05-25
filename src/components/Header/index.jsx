592dbc8d2f58f9a28fb33ea51ea51cdd03b3cc89
Henrique <you@example.com>
2023-05-25 08:17:39 -0300

    index.jsx do Header feito

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+export function Header(){
+    return(
+        <header>
+            <h2>Portfólio</h2>
+            <div>
+                <p>Sobre</p>
+                <p>Stack</p>
+                <p>Projetos</p>
+            </div>
+            <button>Contato</button>
+        </header>
+    )
+}
