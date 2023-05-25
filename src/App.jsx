8733ae3b60b1182022b1570eaf22a2f19c9c45d0
Henrique <you@example.com>
2023-05-25 08:36:14 -0300

    index.jsx do AboutMeSection feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header/index.jsx"
 import { BannerSection } from "./components/Sections/BannerSection/index.jsx"
+import { AboutMeSection } from "./components/Sections/AboutMeSection/index.jsx"
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header></Header>
       <BannerSection></BannerSection>
+      <AboutMeSection></AboutMeSection>
     </>
   )
 }
